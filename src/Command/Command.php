d7215f2f0c29335261fda848479ed8407879e007
Nils Langner <nils.langner@phmlabs.com>
2023-04-26 20:07:18 +0200

    new helper stuff

--- src/Command/Command.php
+++ src/Command/Command.php
@@ -4,6 +4,7 @@ namespace FriendsOfWp\DeveloperCli\Command;
 
 use FriendsOfWp\DeveloperCli\Util\OutputHelper;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Command extends SymfonyCommand
@@ -16,5 +17,16 @@ class Command extends SymfonyCommand
     protected function writeInfo(OutputInterface $output, string|array $message): void
     {
         OutputHelper::writeInfoBox($output, $message);
+    }
+
+    /**
+     * Render a table with the statistics.
+     */
+    protected function renderTable(OutputInterface $output, array $headers, array $rows): void
+    {
+        $table = new Table($output);
+        $table->setHeaders($headers);
+        $table->setRows($rows);
+        $table->render();
     }
 }
--- src/Util/ApiHelper.php
+++ src/Util/ApiHelper.php
@@ -3,16 +3,28 @@
 namespace FriendsOfWp\DeveloperCli\Util;
 
 use GuzzleHttp\Client;
+use Symfony\Component\Yaml\Yaml;
 
 abstract class ApiHelper
 {
     /**
      * Send a GET request to an API that returns JSON content.
      */
-    static public function JsonResponseRequest(string $endpoint)
+    static public function JsonResponseRequest(string $endpoint): array
     {
         $client = new Client();
         $response = $client->get($endpoint);
         return json_decode((string)$response->getBody(), true);
+    }
+
+    /**
+     * Send a GET request to an API that returns YAML content.
+     */
+    static public function YamlResponseRequest(string $endpoint): array
+    {
+        $client = new Client();
+        $response = $client->get($endpoint);
+
+        return Yaml::parse($response->getBody());
     }
 }
